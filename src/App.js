facb3a2444559efc45efe73f18e69e4b603a8135
lukasjaronis <jaronisluke13@gmail.com>
2019-12-11 15:46:08 -0500

    added actions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { addFeature, removeFeature } from './actions'
+import { addFeature, removeFeature } from './actions/actions'
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_FEATURE, REMOVE_FEATURE } from '../actions'
+import { ADD_FEATURE, REMOVE_FEATURE } from '../actions/actions'
 
 export const initialState = {
   additionalPrice: 0,
